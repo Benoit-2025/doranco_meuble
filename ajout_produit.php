f1a610dfc7a81ec734510320d96b9309a9a9a342
Benoit-2025 <abenoit237@yahoo.com>
2022-11-17 15:00:52 +0100

    première initialisation

---
+++ ajout_produit.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * 4 status de fichier sur git
+ *      -untrack => les fichiers créé mais on suivis par git
+ *      - tracked => fichier deje suivit par git  mais non envoyé vers le respository
+ *      
+ *      -straged => les fichiers suivi, enrégistré mais non envoyé vers le respository
+ * 
+ *      - commitred => tous les fichiers envoyé et non modifierde mon projet 
+ */
